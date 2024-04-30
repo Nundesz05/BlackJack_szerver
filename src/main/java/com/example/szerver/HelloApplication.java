7bb16a22f5b283d00d6ed08b599d6dc7961a8c54
Nundesz05 <nundesz06@gmail.com>
2024-04-30 19:28:25 +0200

    végleges verzió

--- src/main/java/com/example/szerver/HelloApplication.java
+++ src/main/java/com/example/szerver/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.szerver;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,7 +13,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("2C.png")));
+        stage.setTitle("Szerver");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/szerver/HelloController.java
+++ src/main/java/com/example/szerver/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
     @FXML private ListView<String> lvList;
     DatagramSocket socket = null;
 
-    private String ip = "25.30.182.43";
+
 
     private int port=678;
 
@@ -19,7 +19,7 @@ public class HelloController {
     public void initialize(){
         try {
             socket = new DatagramSocket(678);
-            kuld("join:5000",ip,port);
+
         } catch (SocketException e) {
             e.printStackTrace();
         }
@@ -34,7 +34,9 @@ public class HelloController {
     }
 
     ArrayList<String> Szerverlapok = new ArrayList<>();
+    ArrayList<String> Szerverlapokertek = new ArrayList<>();
     ArrayList<String> Klienslapok = new ArrayList<>();
+
 
     private String lapbetu[]={"C","D","H","S"};
     private String lapszam[]={"2","3","4","5","6","7","8","9","A","K","J","Q"};
@@ -53,30 +55,21 @@ public class HelloController {
     private int ertek(ArrayList<String> lapok) {
         int ertek=0;
 
+
         for(int i =0;i<lapok.size();i++ ) {
-            if(lapok.get(i).split("")[0].equals("A")) {
+            if(lapok.get(i).equals("A")) {
                 if (ertek + 11 > 21) {
                     ertek += 1;
                 } else {
                     ertek += 11;
                 }
             }
-            else if(lapok.contains(lapok.get(i).split("")[0].equals("A")) && lapok.get(i).split("")[0].equals("Q") || lapok.get(i).split("")[0].equals("J") || lapok.get(i).split("")[0].equals("K")) {
-                if(ertek+ 10>21) {
-                    ertek+=1;
-                } else {
-                    ertek += 10;
-                }
-
-            }
-            else if(lapok.get(i).split("")[0].equals("Q") || lapok.get(i).split("")[0].equals("J") || lapok.get(i).split("")[0].equals("K")) {
-                System.out.println(lapok.get(i));
+            else if(lapok.get(i).equals("Q") || lapok.get(i).equals("J") || lapok.get(i).equals("K")) {
                 ertek += 10;
             }
 
             else {
-                ertek+=Integer.parseInt(lapok.get(i).split("")[0]);
-                System.out.println(lapok.get(i));
+                ertek+=Integer.parseInt(lapok.get(i));
             }
         }
 
@@ -103,10 +96,41 @@ public class HelloController {
             try {
                 socket.receive(packet);
                 String uzenet = new String(packet.getData(), 0, packet.getLength(), "utf-8");
-                Platform.runLater(() -> onFogad(uzenet));
+                String ip = packet.getAddress().getHostAddress();
+                Platform.runLater(() -> onFogad(uzenet,ip));
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void osztas(String ip){
+        kliensbalance=kliensbalance-klienstet;
+        if (ertek(Szerverlapok) >21) {
+            kuld("ertekS:"+String.valueOf(ertek(Szerverlapok)),ip,port);
+            kuld("balance:"+(kliensbalance+(klienstet*2)),ip,port);
+            kliensbalance=kliensbalance+klienstet*2;
+            kuld("end",ip,port);
+            kuld("end",ip,port);
+
+        } else {
+            kuld("ertekS:"+String.valueOf(ertek(Szerverlapok)),ip,port);
+            if(ertek(Klienslapok)> ertek(Szerverlapok)) {
+                kuld("balance:" + (kliensbalance + (klienstet*2)), ip, port);
+                kliensbalance=kliensbalance+(klienstet*2);
+                kuld("end", ip, port);
+            }
+            else if(ertek(Klienslapok)==ertek(Szerverlapok)) {
+
+                kuld("balance:" + (kliensbalance+klienstet), ip, port);
+                kuld("end", ip, port);
+                kliensbalance=kliensbalance+klienstet;
+
+            } else {
+                kuld("balance:"+(kliensbalance),ip,port);
+                kuld("end",ip,port);
+            }
+
         }
     }
 
@@ -115,28 +139,26 @@ public class HelloController {
     private int kliensbalance=0;
 
 
-    private void onFogad(String uzenet) {
+    private void onFogad(String uzenet,String ip) {
         String[] u = uzenet.split(":");
         if(u.length>1) {
             if(u[0].equals("join")) {
                 lvList.getItems().add(uzenet);
-                System.out.println(ip);
                 klienszseton=Integer.parseInt(u[1]);
                 kuld("joined:"+u[1],ip, port);
+                kliensbalance=klienszseton;
 
             }
             else if(u[0].equals("bet")) {
                 Klienslapok.clear();
                 Szerverlapok.clear();
                 klienstet=Integer.parseInt(u[1]);
-                kliensbalance=klienstet;
                 RandomLap(Szerverlapok);
                 kuld("s:"+lap, ip, port);
                 RandomLap(Klienslapok);
                 kuld("k:"+lap, ip, port);
                 RandomLap(Klienslapok);
                 kuld("k:"+lap, ip, port);
-
                 kuld("ertekK:"+String.valueOf(ertek(Klienslapok)),ip,port);
                 kuld("ertekS:"+String.valueOf(ertek(Szerverlapok)),ip,port);
             }
@@ -148,6 +170,7 @@ public class HelloController {
                 kuld("ertekK:"+String.valueOf(ertek(Klienslapok)),ip,port);
                 if(ertek(Klienslapok) >21) {
                     kuld("balance:"+(kliensbalance-klienstet),ip,port);
+                    kliensbalance=kliensbalance-klienstet;
                     kuld("end",ip,port);
                 }
             }
@@ -155,30 +178,8 @@ public class HelloController {
                 while (ertek(Szerverlapok) < 17) {
                     RandomLap(Szerverlapok);
                     kuld("s:"+lap, ip, port);
-                    if (ertek(Szerverlapok) >21) {
-                        kuld("ertekS:"+String.valueOf(ertek(Szerverlapok)),ip,port);
-                        kuld("balance:"+(kliensbalance+(klienstet)),ip,port);
-                        kuld("end",ip,port);
-
-                    } else {
-                        kuld("ertekS:"+String.valueOf(ertek(Szerverlapok)),ip,port);
-                        if(ertek(Klienslapok)> ertek(Szerverlapok)) {
-                            kuld("balance:" + (kliensbalance + (klienstet)), ip, port);
-                            kuld("end", ip, port);
-                        }
-                        else if(ertek(Klienslapok)==ertek(Szerverlapok)) {
-
-                            kuld("balance:" + (kliensbalance), ip, port);
-                            kuld("end", ip, port);
-
-                        } else {
-                            kuld("balance:"+(kliensbalance-klienstet),ip,port);
-                            kuld("end",ip,port);
-                        }
-
-                    }
-
                 };
+                osztas(ip);
 
             }
             if(uzenet.equals("exit")) {
